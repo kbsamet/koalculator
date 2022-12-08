e89ad9637be89170516928886f8631914688e536
SergioCikilot <62554373+SergioCikilot@users.noreply.github.com>
2022-12-08 15:57:31 +0300

    refactor

--- lib/services/images.dart
+++ lib/services/images.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_storage/firebase_storage.dart';
 
+final storage = FirebaseStorage.instance.ref();
 Future<String?> getImage(
   String? pic,
 ) async {
@@ -8,5 +9,15 @@ Future<String?> getImage(
         await FirebaseStorage.instance.refFromURL(pic!).getDownloadURL();
 
     return url;
+  }
+}
+
+Future<String?> getProfilePic(String? id) async {
+  try {
+    String url = await storage.child("profilePics/${id}").getDownloadURL();
+
+    return url;
+  } catch (e) {
+    print(e);
   }
 }
